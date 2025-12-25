bacde59f3cd78593b83cfec90365d16df71be4f3
Nolan A Milligan <noamilligan@NolanMAC.local>
2025-12-24 19:23:09 -0500

    c permutation updated

--- CFiles/Ccode/permutationswap.c
+++ CFiles/Ccode/permutationswap.c
@@ -1,77 +1,39 @@
-#include <stdlib.h>
 #include <stdio.h>
-
-
-char **used;
-int wordcount=0;
+#include <string.h>
 
 
 
+void swap (char *one, char *two){
+    char tmp = *one;
+    *one = *two;
+    *two = tmp;
 
-void swap(char *x, char *y){
-    
-    char temp = *x;
-    *x = *y;
-    *y = temp;
 }
 
-int checkused(char *word){
-    for(int i=0; i<wordcount; i++ ){
-        if (word == used[i]){
-            return 1;
+void permutate(int x, char *word, int len){
+    if (x ==1){
+        printf("%.*s  \n",len, word);
+        return;
+    }
+    else{
+        for (int i= 0; i<x; i++){
+            permutate(x-1, word, len);
+            
+            if(x%2 == 0 ){
+                swap(&word[i], &word[x-1]);
+            }
+            else{            
+                swap(&word[0], &word[x-1]);
+            }
         }
-
-    }
-    return 0;
-}
-
-
-
-
-void perm(char *word, int start, int end){
-    int i;
-    if(start == end && checkused(word)== 0){
         
-        used[wordcount] = word;
-        wordcount +=1;
-        printf("%s \n",word);
-
-    }
-    else {
-        
-        for (i = start; i <= end; i++) {
-        
-            swap((word + start), (word + i));
-
-            perm(word, start + 1, end);
-
-            swap((word + start), (word + i));
-        }
     }
 }
-
-
-
-
 
 int main() {
-    int wordlen;
-    
-
-    
-    
-    printf("how many letters in your perm? \n>");
-    scanf("%d",&wordlen);
-
-    
-
-
-    printf("give the letters in your perm \n>");
-    char *letters = malloc((wordlen+1)*sizeof(char)) ;
-    scanf("%s", letters);
-    
-    perm(letters, 0 , wordlen-1);
-    
+    char word [] = "Nolan";
+    int x = (int)strlen(word);
+    permutate(x,word,x);
 
     return 0;
 }
--- PyFiles/yadyprac.py
+++ PyFiles/yadyprac.py
@@ -1,11 +1,24 @@
 
 
-x= "hi"
+class lego:
+    def __init__(self, name , piece_count):
+        self.piece_count = piece_count
+        print(f"the {name} lego set has arrived and has {piece_count} pieces")
 
-def printx(y,z= "."):
-    word = str.replace(y,"hi","gay")
-    word = str.upper(word)
-    print(word)
+    def build_time(self):
+        smartness = int(input('what is you IQ?\n>'))
+        build_time = 7
+        if smartness < 100:
+            build_time = 12
+        elif smartness > 200:
+            build_time = 5
+        print(f'based on your IQ, it will take you {build_time} hours to complete this set')
+        
+
+        
 
 
-printx(x)
+death_star = lego('death star', 300)
+
+print(death_star.piece_count)
+death_star.build_time()
