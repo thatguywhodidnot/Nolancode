c89e7a4561370fbe231a65a92f5aca24bf1f604f
Nolan A Milligan <noamilligan@NolanMAC.local>
2026-03-06 14:03:02 -0500

    linked list prac

--- githubsave.sh
+++ githubsave.sh
@@ -1,8 +1,4 @@
-
-
 #!/bin/bash
-
-
 
 if [ -z "$1" ]; then
     echo "Error: No commit message supplied."
@@ -14,6 +10,8 @@ git init
 git add .
 git commit -m "$1"
 git branch -M main
-git remote add origin git@github.com:thatguywhodidnot/Nolancode.git 
-git push -u origin main --force
 
+git remote remove origin 2>/dev/null
+git remote add origin git@github.com:thatguywhodidnot/Nolancode.git
+
+git push -u origin main --force
