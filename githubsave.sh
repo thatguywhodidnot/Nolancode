c93ec56213c1cb6d8e04c331fcd7d5284ea0db9c
Nolan A Milligan <noamilligan@NolanMAC.local>
2026-03-06 14:01:20 -0500

    linked lists prac

--- githubsave.sh
+++ githubsave.sh
@@ -14,6 +14,6 @@ git init
 git add .
 git commit -m "$1"
 git branch -M main
-git remote add origin https://github.com/thatguywhodidnot/Nolancode.git
+git remote add origin git@github.com:thatguywhodidnot/Nolancode.git 
 git push -u origin main --force
 
