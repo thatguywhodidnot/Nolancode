7244135c4b311d3b1d1f17395af12a560a0953ea
Super User <thatguywhodidnot@gmail.com>
2026-03-21 08:08:33 -0400

    moving_dot.c initializing field

---
+++ quicktests/header.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+
+
+typedef unsigned long long uint64;
+typedef unsigned int  uint32;
+typedef unsigned short  uint16;
--- quicktests/moving_dot.c
+++ quicktests/moving_dot.c
@@ -1,12 +1,49 @@
 
-#include <stdio.h>
-#include <stdlib.h>
+#include "header.h"
+
+#define  width 32
+#define  height 32
+
+char field[height][width];
+
+uint16 dot_x;
+uint16 dot_y;
+
+void clear_screen(){
+
+	system("clear");
+
+	for(int i=0; i<height; i++){
+		for(int j=0; j<width; j++){
+			field[i][j] = '_';
+		}
+	}
+
+}
+
+void print_field(){
+
+	for(int i=0; i<height; i++){
+		for(int j=0; j<width; j++){
+			printf("%c",field[i][j]);
+		}
+		printf("\n");
+	}
+
+
+}
+
+void print_dot(uint16 x, uint16 y){
+
+	
+
+
+}
 
 
 
 int main(){
-		
-		printf("   \n");
-		printf("\rooo\n");
 
+	clear_screen();
+	print_field();
 }
