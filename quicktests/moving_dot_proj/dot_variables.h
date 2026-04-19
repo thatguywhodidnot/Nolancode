2070b3f4662984394d8d64d1a758820939c58861
Super User <thatguywhodidnot@gmail.com>
2026-04-19 06:32:36 -0400

    made moving dot into mathematical spinning circle

---
+++ quicktests/moving_dot_proj/dot_variables.h
@@ -0,0 +1,11 @@
+
+#define  width 64
+#define  height 64
+#define  rad 28
+#define  diam 56
+#define  tol 150
+#define  frame_space 50000
+#define  frames 500.0
+#define  full 20
+#define  turns 10
+
