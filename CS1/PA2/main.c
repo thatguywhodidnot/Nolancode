a2a37e82b6735bf8316231b290a5a495db78ecbc
Nolan A Milligan <noamilligan@NolanMAC.local>
2026-06-12 08:29:34 -0400

    working on my logic for cs1 pa2 sorting

--- CS1/PA2/main.c
+++ CS1/PA2/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define NUM_LINES 12
 #define MAX_STR 15
 #define EMPTY -1
 
@@ -37,74 +38,101 @@ customer* create_customer(){
     return customer;
 }
 
-//add item
+//check if a line is empty
+int isEmpty(queue* line){
+    return line->front == NULL;
+}
+
+
+
+customer* peek(queue* line){
+    
+    return line->front->customer;
+
+}
+
+void print_customer(customer* customer, int base_time){
+
+    printf("At time %d, %s left the counter from line %d.\n", base_time, customer->customer->name, customer->customer->line_num);
+
+}
+
+//remove item
+customer* dequeue(queue* line){
+
+    node* temp = line->front;
+    customer* customer = temp->customer;
+
+    line->front = line->front->next;
+    if(isEmpty(line)){
+        line->back = NULL;
+    }
+    
+    free(temp);
+    return customer;
+
+}
+//combine these functions??
+int find_least_smoothie(queue* line_queue, int base_time){
+    int best = 12;
+    for(int i=0; i<NUM_LINES; i++){
+        
+        customer* curr = peek(&line_queue[i]);
+        
+        if(curr != NULL && curr->time_in <= base_time){
+            if(best == 12){
+                best = i;
+            }
+            
+        }
+        
+    }
+    
+
+}
+
+// int find_earliest(){}
+
+void sort_and_print(queue* line_queue){
+
+    int best, curr_time = 0;
+    best = find_least_smoothie(line_queue);
+    if(best = 12){
+        best = find_earliest();
+    }
+
+    customer* curr_customer = dequeue(&line_queue[best]);
+
+    print_customer(curr_customer, curr_time);
+
+
+}
+
+//add items to respective lines
 void enqueue(queue* line_queue){
     
     node* temp = malloc(sizeof(node));
     temp->next = NULL;
-
     temp->customer = create_customer();
 
-    if(line_queue->front == NULL){
-        line_queue->front = temp;
-        line_queue->back = temp;
-    }
-    else if(line_queue->front == line_queue->back){
-        line_queue->back->next = temp;
-        line_queue->back = temp;
-    }
-    else{
-        node* compare = line_queue->front->next;
-        node* prev = line_queue-> front;
+    int line_num = temp->customer->line_num - 1;
 
-        while(compare != NULL && (compare->customer->num_smoothies < temp->customer->num_smoothies || 
-        (compare->customer->num_smoothies == temp->customer->num_smoothies && compare->customer->time_in < temp->customer->time_in))){
-            prev = compare;
-            compare = compare->next;
-        }
-        temp->next = compare;
-        prev->next = temp;
-        
-
+    if(line_queue[line_num].front == NULL){
+        line_queue[line_num].front = temp;
+        line_queue[line_num].back = temp;
+    }
+    else {
+        line_queue[line_num].back->next = temp;
+        line_queue[line_num].back = temp;
     }
 }
 
-void print_queue(queue* line_queue){
-    node* customer = line_queue->front;
+int init(queue* line_queue){
+    for(int i=0; i<NUM_LINES; i++){
+        line_queue[i].front = NULL;
+        line_queue[i].back = NULL;
+    }
     
-    int count = 1, base_time = 0;
-
-    while(customer){
-        if(base_time < customer->customer->time_in){
-            base_time = customer->customer->time_in;
-        }
-        base_time += (30 + (5 * customer->customer->num_smoothies));
-        printf("At time %d, %s left the counter from line %d.\n", base_time, customer->customer->name, customer->customer->line_num);
-        customer = customer->next;
-        count++;
-    }
-}
-
-//remove item
-void dequeue(queue* line_queue){
-
-    if(line_queue->front == NULL){
-        return;
-    }
-
-    node* temp = line_queue->front;
-    line_queue->front = line_queue->front->next;
-
-    free(temp->customer->name);
-    free(temp->customer);
-    free(temp);
-
-}
-
-void init(queue* line_queue){
-
-    line_queue->front = NULL;
-    line_queue->back = NULL;
 
     int num_customers, i;
     scanf("%d", &num_customers);
@@ -113,41 +141,26 @@ void init(queue* line_queue){
     for(i=0; i<num_customers; i++){
         enqueue(line_queue);    
     }
-    print_queue(line_queue);
-    for(i=0; i<num_customers; i++){
-        dequeue(line_queue);    
-    }
+    return num_customers;
+    
 
 }
-
-
-
-//check if list empty
-int isEmpty(queue* line_queue){
-    return line_queue->front == NULL;
-}
-
-
-
-// //get front of list
-// int peek();
 
 // stiil need
-// - array of 12 queues
-// - peek() function
-// - dequeue should return customer, not free customer
-// - processing should choose among 12 line fronts
-// - avoid accessing queue internals outside queue functions
+// find best line function to be written logically
 
 int main(){
 
-    int num_tests, i, base_time = 0;
+    int num_tests, num_customers, i, j, base_time = 0;
     scanf("%d", &num_tests);
 
-    queue* line_queue = malloc(sizeof(queue));
-
     for(i=0; i<num_tests; i++){
-        init(line_queue);
+        queue* line_queue = malloc(NUM_LINES * sizeof(queue));
+
+        num_customers = init(line_queue);
+
+        sort_and_print(line_queue);
+
     }
     free(line_queue);
 
