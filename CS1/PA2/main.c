91a026849f6983ca559344ce38ad53763b8ef7eb
Nolan A Milligan <noamilligan@NolanMAC.local>
2026-06-12 10:59:34 -0400

    fuck pa2

--- CS1/PA2/main.c
+++ CS1/PA2/main.c
@@ -21,31 +21,35 @@ typedef struct queue{
     node* back;
 }queue;
 
+//function to read, malloc,and assign customer data
 customer* create_customer(){
     char name[MAX_STR];
     int t, m, x;
+    customer* curr_customer = malloc(sizeof(customer));
+    
+    scanf("%d %d %15s %d", &t, &m, name, &x);
+    
+    curr_customer->time_in = t;
+    curr_customer->line_num = m;
+    curr_customer->num_smoothies = x;
 
-    customer* customer = malloc(sizeof(customer));
-    
-    scanf("%d %d %s %d", &t, &m, name, &x);
-    
-    customer->time_in = t;
-    customer->line_num = m;
-    customer->num_smoothies = x;
-    
-    customer->name = malloc((strlen(name)+1) * sizeof(char));
-    strcpy(customer->name,name);
-    return customer;
+	//uses buffer string and strcpy instead of reading driect into struct    
+    curr_customer->name = malloc((strlen(name)+1) * sizeof(char));
+    strcpy(curr_customer->name,name);
+    return curr_customer;
 }
 
 //check if a line is empty
 int isEmpty(queue* line){
+
     return line->front == NULL;
+
 }
 
 
-
+//return the front of a line or let you know it is empty
 customer* peek(queue* line){
+
     if(isEmpty(line)){
         return NULL;
     }
@@ -53,6 +57,7 @@ customer* peek(queue* line){
     return line->front->customer;
 
 }
+
 
 void print_customer(customer* customer, int base_time){
 
@@ -62,7 +67,7 @@ void print_customer(customer* customer, int base_time){
 
 //remove item
 customer* dequeue(queue* line){
-
+	//save the customer at the front of the line and reassign teh front to the fronts->next
     node* temp = line->front;
     customer* customer = temp->customer;
 
@@ -70,13 +75,14 @@ customer* dequeue(queue* line){
     if(isEmpty(line)){
         line->back = NULL;
     }
-    
+    //free the node after you have gotten the customer
     free(temp);
     return customer;
 
 }
 
 int find_earliest(queue* line_queue){
+	//after first sorting function if no next customer is found
     int best = 12;
     for(int i=0; i<NUM_LINES; i++){
         
@@ -98,6 +104,9 @@ int find_earliest(queue* line_queue){
 }
 
 int find_next_customer(queue* line_queue, int base_time){
+	//for each line, if the front has a customer compare the customers time to the current time(base_time)
+	//if they have arrived early enough check who has the least smoothie to order
+	//if there is a tie check who arrived first
     int best = 12;
     for(int i=0; i<NUM_LINES; i++){
         
@@ -119,11 +128,12 @@ int find_next_customer(queue* line_queue, int base_time){
             }
         }   
     }
+	//return the index of the line in the set of lines who had the best next customer at front
     return best;
 }
 
 void sort_and_print(queue* line_queue, int num_customers){
-
+	//for each customer check which will be served next 
     int best, i, curr_time = 0;
     for(i=0; i<num_customers; i++){
         
@@ -135,10 +145,13 @@ void sort_and_print(queue* line_queue, int num_customers){
             
         }
 
+		//then dequeue them
         customer* curr_customer = dequeue(&line_queue[best]);
 
+		//add their time to clock
         curr_time += 30 + (5 * curr_customer->num_smoothies);
 
+		//print their checkout and free them
         print_customer(curr_customer, curr_time);
 
         free(curr_customer->name);
@@ -150,13 +163,15 @@ void sort_and_print(queue* line_queue, int num_customers){
 
 //add items to respective lines
 void enqueue(queue* line_queue){
-    
+
+    //create a node read customer into nodes customer space
     node* temp = malloc(sizeof(node));
     temp->next = NULL;
     temp->customer = create_customer();
 
     int line_num = temp->customer->line_num - 1;
-
+	
+	//check if this is the first customer in the line
     if(isEmpty(&line_queue[line_num])){
         line_queue[line_num].front = temp;
         line_queue[line_num].back = temp;
@@ -168,12 +183,13 @@ void enqueue(queue* line_queue){
 }
 
 int init(queue* line_queue){
+	//set the all the lines to NULL
     for(int i=0; i<NUM_LINES; i++){
         line_queue[i].front = NULL;
         line_queue[i].back = NULL;
     }
     
-
+	//read in the amount of customers and enqueue each one
     int num_customers, i;
     scanf("%d", &num_customers);
 
@@ -194,6 +210,10 @@ int main(){
     int num_tests, num_customers, i, j, base_time = 0;
     scanf("%d", &num_tests);
 
+	//or each test malloc a set of 12 lines
+	//init the lines with input values
+	//then go through each customer within the lines, sort their eligibility and print their checkouts
+	//print function will free individual customers
     for(i=0; i<num_tests; i++){
         queue* line_queue = malloc(NUM_LINES * sizeof(queue));
 
